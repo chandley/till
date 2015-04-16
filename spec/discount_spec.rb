45af4a834eb62706a7f006d2dfeda50abb5ac4b2
Chris Handley <github.20.alabama3@spamgourmet.com>
2015-04-16 16:44:39 +0100

    refactgor tests - style

--- spec/discount_spec.rb
+++ spec/discount_spec.rb
@@ -1,8 +1,7 @@
 require 'discount'
 
 describe Discount do
- 
-  let(:till) {double :till, total:50}
+  let(:till) { double :till, total: 50 }
   context 'no items' do
     it 'has zero value' do
       discount = Discount.new
@@ -13,55 +12,58 @@ describe Discount do
 
   context 'fixed discount' do
     it 'can apply 10%' do
-      discount = Discount.new { |till| till.total*0.1}
+      discount = Discount.new { |till| till.total * 0.1 }
       expect(discount.value(till)).to eq(5)
     end
     it 'can apply 20%' do
-      discount = Discount.new { |till| till.total*0.2}
+      discount = Discount.new { |till| till.total * 0.2 }
       expect(discount.value(till)).to eq(10)
     end
     it 'has a description when 10%' do
-      discount = Discount.new { |till| till.total*0.1}
+      discount = Discount.new { |till| till.total * 0.1 }
       expect(discount.description(till)).to eq('10% from $50.00')
     end
     it 'has a description when 20%' do
-      discount = Discount.new { |till| till.total*0.2}
+      discount = Discount.new { |till| till.total * 0.2 }
       expect(discount.description(till)).to eq('20% from $50.00')
     end
   end
 
   context 'discount with limit' do
     it 'has zero value below limit' do
-      discount = Discount.new { |till| till.total <= 60 ? 0 : till.total*0.1 }
+      discount = Discount.new { |till| till.total <= 60 ? 0 : till.total * 0.1 }
       expect(discount.value(till)).to eq(0)
     end
     it 'has normal value above limit' do
-      discount = Discount.new { |till| till.total <= 60 ? 0 : till.total*0.1 }
-      big_till = double :till, total:70
+      discount = Discount.new { |till| till.total <= 60 ? 0 : till.total * 0.1 }
+      big_till = double :till, total: 70
       expect(discount.value(big_till)).to eq(7)
     end
   end
 
   context 'muffin discount' do
-    muffin_calc = Proc.new do |till|
+    muffin_calc = proc do |till|
       discount = 0.1
-      muffins = till.items.select{|item| item.name == 'muffin'}
-      values = muffins.map{|muffin| muffin.price}
-      values.reduce(0,:+)*discount
+      muffins = till.items.select { |item| item.name == 'muffin' }
+      values = muffins.map &:price
+      values.reduce(0, :+) * discount
     end
-    
-    let(:discount){ Discount.new &muffin_calc } 
+
+    let(:tea) { double :item, name: 'tea' }
+    let(:muffin) { double :item, name: 'muffin', price: 10 }
+    let(:discount) { Discount.new &muffin_calc }
+
     it 'does not apply generally' do
-      tea = double :item, name: 'tea'
       till = double :till, items: [tea]
       expect(discount.value(till)).to eq(0)
-    end 
+    end
     it 'applies to a muffin' do
-      muffin = double :item, name: 'muffin', price: 10
       till = double :till, items: [muffin]
       expect(discount.value(till)).to eq(1)
-    end 
+    end
+    it 'applies seletively to multiple mixed items' do
+      till = double :till, items: [muffin, muffin, tea, tea, muffin]
+      expect(discount.value(till)).to eq(3)
+    end
   end
-  
-
 end
