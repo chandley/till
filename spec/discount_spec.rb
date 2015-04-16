f198774952462cc215184aa7d95c0a49b2fc9946
Chris Handley <github.20.alabama3@spamgourmet.com>
2015-04-16 15:42:22 +0100

    Additional thresold test

--- spec/discount_spec.rb
+++ spec/discount_spec.rb
@@ -17,5 +17,17 @@ describe Discount do
     till = double :till, total:50
     expect(discount.value(till)).to eq(10)
   end
+
+  it 'has zero value below threshold' do
+    discount = Discount.new { |till| till.total <= 60 ? 0 : till.total*0.1 }
+    till = double :till, total:50
+    expect(discount.value(till)).to eq(0)
+  end
+  it 'has normal value above threshold' do
+    discount = Discount.new { |till| till.total <= 60 ? 0 : till.total*0.1 }
+    till = double :till, total:70
+    expect(discount.value(till)).to eq(7)
+  end
+
   
 end
